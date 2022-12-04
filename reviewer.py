285bc9308f119de6e3293d705833378266e8779b
Avi <avraham.sikirov@live.biu.ac.il>
2022-12-04 20:55:51 +0200

    limit the ratings

--- reviewer.py
+++ reviewer.py
@@ -2,6 +2,7 @@ import os
 import mysql.connector
 import re
 from dotenv import load_dotenv
+
 load_dotenv()
 password = os.getenv('MYSQL_ROOT_PASSWORD')
 # Create a connection
@@ -15,6 +16,7 @@ cnx = mysql.connector.connect(
 cursor = cnx.cursor()
 cnx.autocommit = True
 
+
 def create_rating_table():
     if check_table_exists("rating"):
         return
@@ -27,6 +29,7 @@ def create_rating_table():
           FOREIGN KEY (reviewer_id) REFERENCES reviewer(reviewer_id)
         );
     """)
+
 
 def create_reviewer_table():
     if check_table_exists("reviewer"):
@@ -47,42 +50,49 @@ def check_table_exists(table_name):
 
 
 insert_to_reviewer_stm = "INSERT INTO reviewer (first_name, last_name) VALUES (%s, %s)"
+
+
 def find_customer_by_id(id):
     cursor.execute(f"SELECT * FROM reviewer WHERE reviewer_id= {id};")
     result = cursor.fetchall()
     return result
+
 
 def add_reviewer_by_name():
     first_name = input("Please enter your first name: ")
     last_name = input("Please enter your last name: ")
     cursor.execute(insert_to_reviewer_stm, (first_name, last_name))
     id = cursor.lastrowid
-    return [(id,first_name, last_name)]
+    return [(id, first_name, last_name)]
+
 
 def film_exists(film_name):
     cursor.execute(f"SELECT * FROM film WHERE title = '{film_name}';")
     result = cursor.fetchall()
     return result
 
+
 def is_rating_valid(rating):
     pattern = re.compile(r"^\d\.\d$")
     return pattern.match(rating)
+
 
 def print_rating():
     cursor.execute("""
             select f.title, concat(rev.first_name, " ", rev.last_name), r.rating
             from rating as r, film as f, reviewer as rev
             where r.film_id = f.film_id AND r.reviewer_id = rev.reviewer_id
-            
+            limit 100            
         """)
     ratings = cursor.fetchall()
     for rate in ratings:
         print("Film name:", rate[0] + ",", "Full name:", rate[1] + ",", "Rating:", float(rate[2]))
 
 
-
 insert_to_rating_stm = "INSERT INTO rating (film_id, reviewer_id, rating) VALUES (%s, %s, %s)"
 update_to_rating_stm = "UPDATE rating SET rating = %s WHERE film_id = %s AND reviewer_id = %s"
+
+
 def add_rating(film_id, reviewer_id, rating):
     cursor.execute(f"SELECT * FROM rating WHERE film_id = '{film_id}' AND reviewer_id = '{reviewer_id}';")
     result = cursor.fetchall()
@@ -92,20 +102,19 @@ def add_rating(film_id, reviewer_id, rating):
         cursor.execute(update_to_rating_stm, (rating, film_id, reviewer_id))
 
 
-
 create_reviewer_table()
 create_rating_table()
 
 customer_id = input("Please enter your id: ")
 result = find_customer_by_id(customer_id)
-if(result == []):
+if (result == []):
     result = add_reviewer_by_name()
 print("Hello,", result[0][1], result[0][2] + ".")
 
 film_name = input("Please enter film name to rate: ")
 films = []
 film_id = ""
-while(films == []):
+while (films == []):
     films = film_exists(film_name)
     if (len(films) == 1):
         film_id = films[0][0]
